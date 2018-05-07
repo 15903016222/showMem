5cd63baa645003530278e17c6d7538dacdc92c24
wangsheng <wangsheng@cndoppler.cn>
2018-05-07 14:53:50 +0800

    添加：获取仪器存储信息脚本

---
+++ getMem.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+FILE="/tmp/demo.xml"
+
+write_xml()
+{
+	echo "<disk>" >> $FILE
+	echo "<name>$1</name>" >> $FILE
+	echo "<used>$2</used>" >> $FILE
+	echo "<free>$3</free>" >> $FILE
+	echo "</disk>" >> $FILE
+}
+
+get_disk_info()
+{
+	disk=`cat /proc/mounts | grep "$1" | awk '{print $2}'`
+	used=`df $disk | grep "$disk" | awk '{print $3}'`
+	free=`df $disk | grep "$disk" | awk '{print $4}'`
+	if [ "/" == "$disk" ]
+	then
+		disk="system"
+	else
+		disk=${disk##*/}
+	fi
+}
+
+
+test -f "$FILE" && rm "$FILE"
+
+contents=`cat /proc/mounts | awk '{print $1}' | grep "/dev/"`
+
+echo "<disks>" > $FILE
+for content in `echo $contents`
+do
+	get_disk_info $content
+	write_xml $disk $used $free
+done
+echo "</disks>" >> $FILE
