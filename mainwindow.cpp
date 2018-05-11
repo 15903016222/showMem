26a4359307d8ebb6d843460f5cb3d8576f17ac1b
wangsheng <wangsheng@cndoppler.cn>
2018-05-11 11:45:11 +0800

    取消主窗口的菜单栏，状态栏，工具栏

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     showMemDialog *dialog = new showMemDialog(this);
     ui->centralWidget->setStyleSheet("background-color:black;");
+    ui->mainToolBar->deleteLater();
+    ui->menuBar->deleteLater();
+    ui->statusBar->deleteLater();
     connect(ui->pushButton, SIGNAL(clicked(bool)), dialog, SLOT(exec()));
 }
 
